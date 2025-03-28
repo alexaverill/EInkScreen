7d70dfaab8e092e6a024658250f25442d1a83188
alexaverill <alex.averill2013@gmail.com>
2025-03-27 20:16:48 -0500

    Adding in a resize endpoint that will display on screen

--- Controllers/PictureContriller.cs
+++ Controllers/PictureContriller.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace EInkFrame.Controllers;
-
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 [ApiController]
 [Route("[controller]")]
 public class PictureController : ControllerBase
@@ -24,7 +25,6 @@ public class PictureController : ControllerBase
     public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
     {
         long size = files.Sum(f => f.Length);
-        Console.WriteLine(files.Count());
         var formFile = files[0];
         //note this is only ever runnin internal to network, not too worried about malicious attempts at the mometn
         var imageFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "images");
@@ -40,5 +40,27 @@ public class PictureController : ControllerBase
         //Call Inky to write image to 
 
         return Ok(new { count = files.Count, size });
+    }
+    [Route("resize")]
+    [HttpPost]
+    public async Task<IActionResult> UploadAndResize(List<IFormFile> files)
+    {
+        var finalWidth = 600;
+        var finalHeight = 448;
+        using MemoryStream imageStream = new();
+        await files[0].CopyToAsync(imageStream);
+        imageStream.Position = 0;
+        using var image = Image.Load(imageStream);
+        image.Mutate(img => img.Resize(new ResizeOptions
+        {
+            Mode = ResizeMode.Pad,
+            Size = new Size() { Height = finalHeight }
+        }));
+        var imageFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "images");
+        Directory.CreateDirectory(imageFolder);
+        var filePath = Path.Combine(imageFolder, files[0].FileName);
+        image.Save(filePath);
+        _screen.SetImage(filePath);
+        return Ok();
     }
 }
